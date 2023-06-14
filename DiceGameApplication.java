9dd7294d46d8f88ba04cd10dab4ac535b815e9e0
上島頼人 <raitokamishima@wlan-napt-004.u-aizu.ac.jp>
2023-06-14 16:32:23 +0900

    Greetings added

--- DiceGameApplication.java
+++ DiceGameApplication.java
@@ -1,10 +1,16 @@
 import java.util.Random;
+import java.util.Scanner;
 class DiceGameApplication{
     public static void main(String args[]){
         Random random = new Random();
+        Scanner sc = new Scanner(System.in);
         int n=2;
         int total=0;
         int[] dices = new int[n];
+        System.out.println("What is your name?");
+        System.out.print("> ");
+        String name = sc.next();
+        System.out.println("Hello,"+name+"!");
         System.out.println("Rolling dice...");
         for(int i=0;i<n;i++){
             int die = random.nextInt(6)+1;
