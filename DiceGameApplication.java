da9fd889d1508afbe5082f808e0449f9f991debb
上島頼人 <raitokamishima@wlan-napt-004.u-aizu.ac.jp>
2023-06-14 15:58:03 +0900

    Dice game: initial revision

---
+++ DiceGameApplication.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+class DiceGameApplication{
+    public static void main(String args[]){
+        Random random = new Random();
+        int n=2;
+        int total=0;
+        int[] dices = new int[n];
+        System.out.println("Rolling dice...");
+        for(int i=0;i<n;i++){
+            int die = random.nextInt(6)+1;
+            dices[i]=die;
+            System.out.println("Die "+(i+1)+": "+dices[i]);
+            total+=dices[i];
+        }
+        System.out.println("Total value: "+total);
+    }
+}
